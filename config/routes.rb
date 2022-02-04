da216d35db493893f2fdc46a15ace27b6020d635
AnastasiiaBilynska <nk015132@gmail.com>
2022-02-04 23:13:57 +0200

    lesson 9 has been completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[new create]
-  
+  resources :users, only: %i[new create edit update]
+  resource :session, only: %i[new create destroy]
+
   resources :questions do
     resources :answers, except: %i[new show]
   end
